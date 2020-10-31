5acfcc4c519e0e175ab73976c00a9579f64dc34a
Douglas Eleutério <douglaseleuterio@gmail.com>
2020-10-31 17:04:53 -0300

    Criando melhores respostas para api

--- serverless/src/controller/user/index.js
+++ serverless/src/controller/user/index.js
@@ -1,11 +1,17 @@
 const createService = require('./service/create');
 
 async function create(body) {
-    const responseBody = await createService(body);
-    return {
-        statusCode: 200,
-        body: responseBody,
-    };
+    const { name, email, phone, address } = body;
+    if (!name || !email || !phone) {
+        return { statusCode: 422, details: 'Missing information' };
+    } 
+    try {
+        const userDTO = { name, email, phone, address };
+        const responseBody = await createService({ userDTO });
+        return { statusCode: 200, body: responseBody };
+    } catch (error) {
+        return { statusCode: 500, stackTrace: error };
+    }
 }
 
 module.exports = { create };
