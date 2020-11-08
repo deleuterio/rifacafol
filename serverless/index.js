fd61571f2ff12f46250f24540dd694f3313c36f8
Douglas Eleutério <douglaseleuterio@gmail.com>
2020-11-08 15:33:35 -0300

    Preparando fila SQS para processar mensagens

--- serverless/index.js
+++ serverless/index.js
@@ -1,2 +1,4 @@
-const serverless = require('./src/stack/serverless');
-module.exports.handler = serverless;
+const app = require('./src/stack/serverless/app');
+const processor = require('./src/stack/serverless/processor');
+module.exports.handler = app;
+module.exports.processor = processor;
