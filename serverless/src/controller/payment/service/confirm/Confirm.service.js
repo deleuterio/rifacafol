7fa8929373478f82ffcb7aad4d12870655ddaf29
Douglas Eleutério <douglaseleuterio@gmail.com>
2020-11-12 21:12:01 -0300

    Mapeando evento do webhook paypal

--- serverless/src/controller/payment/service/confirm/Confirm.service.js
+++ serverless/src/controller/payment/service/confirm/Confirm.service.js
@@ -6,9 +6,20 @@ class PaymentConfirmService {
     async execute({ body }) {
         const { resource } = body;
 
+        const paymentConfirmDTO = {
+            eventId: body.id,
+            created: body.create_time;
+            resourceType: body.resource_type,
+            eventType: body.event_type,
+            orderId: resource.id,
+            amount: resource.amount,
+            finalCapture: resource.final_capture,
+            orderStatus: resource.status
+        };
+
         try {
             return await this.rafflePaymentSuccessQueue.send({
-                body: JSON.stringify(body),
+                body: JSON.stringify(paymentConfirmDTO),
                 messageId: resource.id,
             });
         } catch (error) {
