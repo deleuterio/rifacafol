f1f3d8497a3c6d9fee3b652430099b83e75eb3de
Douglas <douglaseleuterio@gmail.com>
2020-11-19 21:56:52 -0300

    Fazendo getobject dos resultados do dia

--- serverless/src/controller/raffle/service/createReport/CreateReport.service.js
+++ serverless/src/controller/raffle/service/createReport/CreateReport.service.js
@@ -4,19 +4,35 @@ class RaffleCreateReportService {
         this.remoteKey = 'raffle/orders';
         this.now = new Date();
     }
+
     async execute({ date }) {
         if (date) {
             this.now = date;
         }
-        const urls = await this._getObjects(this.now);
-        return urls;
+        const s3Keys = await this._getObjects(this.now);
+        if (s3Keys) {
+            const raffleOrders = await Promise.all(
+                s3Keys.map(async key => {
+                    const raffleOrder = this.rifaDatalakeRawFileStorage.getJSON({ key });
+                    return raffleOrder;
+                }),
+            );
+            return raffleOrders;
+        }
     }
 
+    /**
+     * @private
+     * @description Get a list of s3 keys from requested date
+     * @param {Date} date The date to create report
+     * @return {Array.String} The keys
+     */
     async _getObjects(date) {
         const remotePath = `${this.remoteKey}/year=${date.getFullYear()}/month=${date.getMonth() + 1}/day=${date.getDate()}`;
         const key = this.rifaDatalakeRawFileStorage.createRemotePath(remotePath);
         try {
-            return await this.rifaDatalakeRawFileStorage.listObjects({ key });
+            const { Contents } = await this.rifaDatalakeRawFileStorage.listObjects({ key });
+            return Contents.map(({ Key }) => Key);
         } catch (error) {
             console.error(error);
             throw error;
