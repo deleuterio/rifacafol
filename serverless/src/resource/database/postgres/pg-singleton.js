f4f8b61bf6175c3611c02b924585e57c5ffdbbc3
Douglas <douglaseleuterio@gmail.com>
2020-11-22 10:01:22 -0300

    Add teste de conexão com database

--- serverless/src/resource/database/postgres/pg-singleton.js
+++ serverless/src/resource/database/postgres/pg-singleton.js
@@ -2,4 +2,29 @@ const { Pool } = require('pg');
 
 const pgPool = new Pool();
 
-module.exports = { pgPool };
+const psqlClient = {
+    async query(text, values) {
+        const client = await pgPool.connect();
+        const result = await client.query(text, values);
+        client.release();
+        return result.rows;
+    },
+
+    async testConnection() {
+        try {
+            const client = await pgPool.connect();
+            const result = await client.query('SELECT now()');
+            client.release();
+            return result.rows;
+        } catch (error) {
+            console.error(error)
+            throw error;
+        }
+    },
+};
+
+psqlClient.testConnection().then(() => {
+    console.log('Database connected');
+});
+
+module.exports = { psqlClient };
